3680c8aec1d2afa9132b90e27914941651dd3cc1
Xapadoan <HollyolGamu@hotmail.com>
2024-01-28 11:05:21 +0100

    Dialog as component

--- src/components/ContactDialog.tsx
+++ src/components/ContactDialog.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import cn from 'classnames';
-import * as Dialog from '@radix-ui/react-dialog';
+import { Dialog } from '@components/Dialog';
 import { useForm } from 'react-hook-form';
 import { cutiveMono } from '@styles/fonts';
 
@@ -22,64 +22,48 @@ export const ContactDialog = ({ trigger }: { trigger: ReactNode }) => {
     console.log('Submit Contact: ', data);
   };
   return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content className="fixed bottom-0 right-0 h-screen w-[50vw] border-gray-400 border-2">
-          <header className="flex flex-row align-middle bg-gray-700 border-b-1 border-gray-400">
-            <Dialog.Close className="outline-none focus:outline-none">
-              <div className="flex justify-center items-center w-6 h-6 text-[10px]">
-                🔴
-              </div>
-            </Dialog.Close>
-            <Dialog.Title className="w-full text-center">
-              ./send-mail.sh
-            </Dialog.Title>
-          </header>
-          <form
-            className={cn('text-[18px] font-bold p-3', cutiveMono.className)}
-            onSubmit={formContext.handleSubmit(onSubmit)}
-          >
-            <div className="flex flex-row gap-4 font-bold">
-              <label htmlFor="from">From:</label>
-              <input
-                className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
-                type="email"
-                {...formContext.register('from')}
-              />
-            </div>
-            <div className="flex flex-row gap-4 font-bold">
-              <label>To:</label>
-              <span className="font-normal">xavier.padoan.dev@gmail.com</span>
-            </div>
-            <div className="flex flex-row gap-4 font-bold">
-              <label>Date:</label>
-              <span className="font-normal">{new Date().toUTCString()}</span>
-            </div>
-            <div className="flex flex-row gap-4 font-bold">
-              <label htmlFor="subject">Subject:</label>
-              <input
-                className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
-                type="subject"
-                {...formContext.register('subject')}
-              />
-            </div>
-            <div>
-              <textarea
-                className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
-                rows={10}
-                {...formContext.register('body')}
-              />
-            </div>
-            <input
-              className="underline decoration-dotted underline-offset-2 hover:cursor-pointer hover:text-blue-600"
-              type="submit"
-              value="Send"
-            />
-          </form>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+    <Dialog title="send-mail" trigger={trigger}>
+      <form
+        className={cn('text-[18px] font-bold p-3', cutiveMono.className)}
+        onSubmit={formContext.handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-row gap-4 font-bold">
+          <label htmlFor="from">From:</label>
+          <input
+            className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
+            type="email"
+            {...formContext.register('from')}
+          />
+        </div>
+        <div className="flex flex-row gap-4 font-bold">
+          <label>To:</label>
+          <span className="font-normal">xavier.padoan.dev@gmail.com</span>
+        </div>
+        <div className="flex flex-row gap-4 font-bold">
+          <label>Date:</label>
+          <span className="font-normal">{new Date().toUTCString()}</span>
+        </div>
+        <div className="flex flex-row gap-4 font-bold">
+          <label htmlFor="subject">Subject:</label>
+          <input
+            className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
+            type="subject"
+            {...formContext.register('subject')}
+          />
+        </div>
+        <div>
+          <textarea
+            className="bg-black focus:outline-none hover:bg-gray-800 focus:bg-gray-900 font-normal w-full"
+            rows={10}
+            {...formContext.register('body')}
+          />
+        </div>
+        <input
+          className="underline decoration-dotted underline-offset-2 hover:cursor-pointer hover:text-blue-600"
+          type="submit"
+          value="Send"
+        />
+      </form>
+    </Dialog>
   );
 };
---
+++ src/components/Dialog.tsx
@@ -0,0 +1,38 @@
+import {
+  Root,
+  Trigger,
+  Portal,
+  Content,
+  Close,
+  Title,
+} from '@radix-ui/react-dialog';
+import { ReactNode } from 'react';
+
+export const Dialog = ({
+  title,
+  trigger,
+  children,
+}: {
+  title: string;
+  trigger: ReactNode;
+  children: ReactNode;
+}) => {
+  return (
+    <Root>
+      <Trigger asChild>{trigger}</Trigger>
+      <Portal>
+        <Content className="fixed bottom-0 right-0 h-screen w-[50vw] border-gray-400 border-2">
+          <header className="flex flex-row align-middle bg-gray-700 border-b-1 border-gray-400">
+            <Close className="outline-none focus:outline-none">
+              <div className="flex justify-center items-center w-6 h-6 text-[10px]">
+                🔴
+              </div>
+            </Close>
+            <Title className="w-full text-center">{`./${title}.sh`}</Title>
+          </header>
+          {children}
+        </Content>
+      </Portal>
+    </Root>
+  );
+};
